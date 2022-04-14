bfc93de1d445f0975ff0ed99cd5f8a79e6956417
Aniruddha-Deb <aniruddha.deb.2002@gmail.com>
2022-04-15 00:31:16 +0530

    Working PoC done

--- task2/assets/tilesets/iitd.tsx
+++ task2/assets/tilesets/iitd.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.2" name="iitd" tilewidth="16" tileheight="16" tilecount="4096" columns="64">
- <image source="../raw/self/iitd.png" width="1024" height="1024"/>
+<tileset version="1.8" tiledversion="1.8.2" name="iitd" tilewidth="16" tileheight="16" tilecount="4096" columns="23">
+ <image source="../raw/self/iitd.png" width="368" height="288"/>
 </tileset>
